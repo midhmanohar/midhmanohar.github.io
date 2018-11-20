6cc231750420709933aaa65880a2450ee0177bc0
midhun manohar <midhmanohar@gmail.com>
2018-11-20 12:31:27 +0530

    added home page

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#preloader').delay(10000).fadeOut();
+});
+$(document).ready(function(){
+    var typed = new Typed("#typed", {
+        // Waits 1000ms after typing "First"
+        strings: ["Midhun"],
+        typeSpeed : 150,
+        showCursor : false,
+      });
+});
